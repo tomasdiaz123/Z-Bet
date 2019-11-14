dc23404d8b32d0767c587f1f97958656aaddc5ca
tomasdiaz123 <57632207+tomasdiaz123@users.noreply.github.com>
2019-11-14 13:40:39 +0000

    Add files via upload

---
+++ ZeBet-BackOffice/ZeBet-BackOffice/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ZeBet_BackOffice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbcTabs = new System.Windows.Forms.TabControl();
+            this.tpLogin = new System.Windows.Forms.TabPage();
+            this.tpRegister = new System.Windows.Forms.TabPage();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tbcTabs.SuspendLayout();
+            this.tpLogin.SuspendLayout();
+            this.tpRegister.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbcTabs
+            // 
+            this.tbcTabs.Controls.Add(this.tpLogin);
+            this.tbcTabs.Controls.Add(this.tpRegister);
+            this.tbcTabs.Location = new System.Drawing.Point(1, 12);
+            this.tbcTabs.Name = "tbcTabs";
+            this.tbcTabs.SelectedIndex = 0;
+            this.tbcTabs.Size = new System.Drawing.Size(267, 246);
+            this.tbcTabs.TabIndex = 1;
+            // 
+            // tpLogin
+            // 
+            this.tpLogin.Controls.Add(this.label3);
+            this.tpLogin.Controls.Add(this.btnLogin);
+            this.tpLogin.Controls.Add(this.label2);
+            this.tpLogin.Controls.Add(this.label1);
+            this.tpLogin.Controls.Add(this.checkBox1);
+            this.tpLogin.Controls.Add(this.txtPass);
+            this.tpLogin.Controls.Add(this.txtUser);
+            this.tpLogin.Location = new System.Drawing.Point(4, 22);
+            this.tpLogin.Name = "tpLogin";
+            this.tpLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tpLogin.Size = new System.Drawing.Size(259, 220);
+            this.tpLogin.TabIndex = 0;
+            this.tpLogin.Text = "Login";
+            this.tpLogin.UseVisualStyleBackColor = true;
+            // 
+            // tpRegister
+            // 
+            this.tpRegister.Controls.Add(this.label7);
+            this.tpRegister.Controls.Add(this.textBox3);
+            this.tpRegister.Controls.Add(this.label4);
+            this.tpRegister.Controls.Add(this.button1);
+            this.tpRegister.Controls.Add(this.label5);
+            this.tpRegister.Controls.Add(this.label6);
+            this.tpRegister.Controls.Add(this.checkBox2);
+            this.tpRegister.Controls.Add(this.textBox1);
+            this.tpRegister.Controls.Add(this.textBox2);
+            this.tpRegister.Location = new System.Drawing.Point(4, 22);
+            this.tpRegister.Name = "tpRegister";
+            this.tpRegister.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRegister.Size = new System.Drawing.Size(259, 220);
+            this.tpRegister.TabIndex = 1;
+            this.tpRegister.Text = "Register";
+            this.tpRegister.UseVisualStyleBackColor = true;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(95, 97);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(95, 120);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(100, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(199, 126);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(93, 179);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label3.Location = new System.Drawing.Point(100, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Login";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label4.Location = new System.Drawing.Point(89, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Register";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Username:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(211, 123);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Repeat Password:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 270);
+            this.Controls.Add(this.tbcTabs);
+            this.Name = "Form1";
+            this.Text = "ZéBet";
+            this.tbcTabs.ResumeLayout(false);
+            this.tpLogin.ResumeLayout(false);
+            this.tpLogin.PerformLayout();
+            this.tpRegister.ResumeLayout(false);
+            this.tpRegister.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbcTabs;
+        private System.Windows.Forms.TabPage tpLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TabPage tpRegister;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
