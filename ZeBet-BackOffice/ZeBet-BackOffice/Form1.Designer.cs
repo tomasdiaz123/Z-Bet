71a11669d72c7c7d375555e3c06fe67f6f7c7cdc
I170001 <I170001@SINF117>
2019-11-18 15:22:19 +0000

    login and register

--- ZeBet-BackOffice/ZeBet-BackOffice/Form1.Designer.cs
+++ ZeBet-BackOffice/ZeBet-BackOffice/Form1.Designer.cs
@@ -28,28 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tbcTabs = new System.Windows.Forms.TabControl();
             this.tpLogin = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkShowPass1 = new System.Windows.Forms.CheckBox();
             this.txtPass = new System.Windows.Forms.TextBox();
             this.txtUser = new System.Windows.Forms.TextBox();
             this.tpRegister = new System.Windows.Forms.TabPage();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtRepeatPass = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.btnRegister = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chkShowPass2 = new System.Windows.Forms.CheckBox();
+            this.txtPass2 = new System.Windows.Forms.TextBox();
+            this.txtUser2 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.tbcTabs.SuspendLayout();
             this.tpLogin.SuspendLayout();
             this.tpRegister.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // tbcTabs
@@ -68,7 +71,7 @@
             this.tpLogin.Controls.Add(this.btnLogin);
             this.tpLogin.Controls.Add(this.label2);
             this.tpLogin.Controls.Add(this.label1);
-            this.tpLogin.Controls.Add(this.checkBox1);
+            this.tpLogin.Controls.Add(this.chkShowPass1);
             this.tpLogin.Controls.Add(this.txtPass);
             this.tpLogin.Controls.Add(this.txtUser);
             this.tpLogin.Location = new System.Drawing.Point(4, 22);
@@ -97,6 +100,7 @@
             this.btnLogin.TabIndex = 5;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // label2
             // 
@@ -116,14 +120,15 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Username:";
             // 
-            // checkBox1
+            // chkShowPass1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(199, 126);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chkShowPass1.AutoSize = true;
+            this.chkShowPass1.Location = new System.Drawing.Point(199, 126);
+            this.chkShowPass1.Name = "chkShowPass1";
+            this.chkShowPass1.Size = new System.Drawing.Size(15, 14);
+            this.chkShowPass1.TabIndex = 2;
+            this.chkShowPass1.UseVisualStyleBackColor = true;
+            this.chkShowPass1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // txtPass
             // 
@@ -131,6 +136,7 @@
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(100, 20);
             this.txtPass.TabIndex = 1;
+            this.txtPass.UseSystemPasswordChar = true;
             // 
             // txtUser
             // 
@@ -142,14 +148,14 @@
             // tpRegister
             // 
             this.tpRegister.Controls.Add(this.label7);
-            this.tpRegister.Controls.Add(this.textBox3);
+            this.tpRegister.Controls.Add(this.txtRepeatPass);
             this.tpRegister.Controls.Add(this.label4);
             this.tpRegister.Controls.Add(this.btnRegister);
             this.tpRegister.Controls.Add(this.label5);
             this.tpRegister.Controls.Add(this.label6);
-            this.tpRegister.Controls.Add(this.checkBox2);
-            this.tpRegister.Controls.Add(this.textBox1);
-            this.tpRegister.Controls.Add(this.textBox2);
+            this.tpRegister.Controls.Add(this.chkShowPass2);
+            this.tpRegister.Controls.Add(this.txtPass2);
+            this.tpRegister.Controls.Add(this.txtUser2);
             this.tpRegister.Location = new System.Drawing.Point(4, 22);
             this.tpRegister.Name = "tpRegister";
             this.tpRegister.Padding = new System.Windows.Forms.Padding(3);
@@ -167,12 +173,13 @@
             this.label7.TabIndex = 15;
             this.label7.Text = "Repeat Password:";
             // 
-            // textBox3
+            // txtRepeatPass
             // 
-            this.textBox3.Location = new System.Drawing.Point(105, 146);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 14;
+            this.txtRepeatPass.Location = new System.Drawing.Point(105, 146);
+            this.txtRepeatPass.Name = "txtRepeatPass";
+            this.txtRepeatPass.Size = new System.Drawing.Size(100, 20);
+            this.txtRepeatPass.TabIndex = 14;
+            this.txtRepeatPass.UseSystemPasswordChar = true;
             // 
             // label4
             // 
@@ -192,6 +199,7 @@
             this.btnRegister.TabIndex = 12;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // label5
             // 
@@ -211,28 +219,34 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Username:";
             // 
-            // checkBox2
+            // chkShowPass2
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(211, 123);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(15, 14);
-            this.checkBox2.TabIndex = 9;
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.chkShowPass2.AutoSize = true;
+            this.chkShowPass2.Location = new System.Drawing.Point(211, 123);
+            this.chkShowPass2.Name = "chkShowPass2";
+            this.chkShowPass2.Size = new System.Drawing.Size(15, 14);
+            this.chkShowPass2.TabIndex = 9;
+            this.chkShowPass2.UseVisualStyleBackColor = true;
+            this.chkShowPass2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
-            // textBox1
+            // txtPass2
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 120);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 8;
+            this.txtPass2.Location = new System.Drawing.Point(105, 120);
+            this.txtPass2.Name = "txtPass2";
+            this.txtPass2.Size = new System.Drawing.Size(100, 20);
+            this.txtPass2.TabIndex = 8;
+            this.txtPass2.UseSystemPasswordChar = true;
             // 
-            // textBox2
+            // txtUser2
             // 
-            this.textBox2.Location = new System.Drawing.Point(105, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
+            this.txtUser2.Location = new System.Drawing.Point(105, 94);
+            this.txtUser2.Name = "txtUser2";
+            this.txtUser2.Size = new System.Drawing.Size(100, 20);
+            this.txtUser2.TabIndex = 7;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // Form1
             // 
@@ -247,6 +261,7 @@
             this.tpLogin.PerformLayout();
             this.tpRegister.ResumeLayout(false);
             this.tpRegister.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -259,19 +274,20 @@
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox chkShowPass1;
         private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.TabPage tpRegister;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtRepeatPass;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox chkShowPass2;
+        private System.Windows.Forms.TextBox txtPass2;
+        private System.Windows.Forms.TextBox txtUser2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
