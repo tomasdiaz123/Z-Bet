1176790d8618605e89ab6c88a7fef6b7800f2d80
I170001 <I170001@SINF117>
2019-11-28 08:53:30 +0000

    cf

--- zÉbET/backOffice/Form2.Designer.cs
+++ zÉbET/backOffice/Form2.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.tbcTabs = new System.Windows.Forms.TabControl();
             this.tpUsers = new System.Windows.Forms.TabPage();
+            this.txtPass = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,7 +44,6 @@
             this.chNivel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.chSaldo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tpJogos = new System.Windows.Forms.TabPage();
-            this.txtPass = new System.Windows.Forms.TextBox();
             this.tbcTabs.SuspendLayout();
             this.tpUsers.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numSaldo)).BeginInit();
@@ -56,7 +56,7 @@
             this.tbcTabs.Location = new System.Drawing.Point(12, 20);
             this.tbcTabs.Name = "tbcTabs";
             this.tbcTabs.SelectedIndex = 0;
-            this.tbcTabs.Size = new System.Drawing.Size(453, 314);
+            this.tbcTabs.Size = new System.Drawing.Size(662, 400);
             this.tbcTabs.TabIndex = 0;
             // 
             // tpUsers
@@ -74,14 +74,22 @@
             this.tpUsers.Location = new System.Drawing.Point(4, 22);
             this.tpUsers.Name = "tpUsers";
             this.tpUsers.Padding = new System.Windows.Forms.Padding(3);
-            this.tpUsers.Size = new System.Drawing.Size(445, 288);
+            this.tpUsers.Size = new System.Drawing.Size(654, 374);
             this.tpUsers.TabIndex = 0;
             this.tpUsers.Text = "Users";
             this.tpUsers.UseVisualStyleBackColor = true;
             // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(16, 87);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(117, 20);
+            this.txtPass.TabIndex = 9;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(349, 255);
+            this.btnSave.Location = new System.Drawing.Point(128, 312);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 8;
@@ -92,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(195, 56);
+            this.label2.Location = new System.Drawing.Point(140, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 7;
@@ -105,7 +113,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nome";
             // 
             // cbbNivel
             // 
@@ -114,17 +122,17 @@
             this.cbbNivel.Items.AddRange(new object[] {
             "user",
             "admin"});
-            this.cbbNivel.Location = new System.Drawing.Point(16, 72);
+            this.cbbNivel.Location = new System.Drawing.Point(139, 87);
             this.cbbNivel.Name = "cbbNivel";
-            this.cbbNivel.Size = new System.Drawing.Size(121, 21);
+            this.cbbNivel.Size = new System.Drawing.Size(78, 21);
             this.cbbNivel.TabIndex = 5;
             // 
             // btnSend
             // 
             this.btnSend.Enabled = false;
-            this.btnSend.Location = new System.Drawing.Point(331, 69);
+            this.btnSend.Location = new System.Drawing.Point(139, 131);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(93, 23);
+            this.btnSend.Size = new System.Drawing.Size(82, 23);
             this.btnSend.TabIndex = 4;
             this.btnSend.Text = ">>";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -132,7 +140,7 @@
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(331, 33);
+            this.btnReset.Location = new System.Drawing.Point(23, 131);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(93, 23);
             this.btnReset.TabIndex = 3;
@@ -142,21 +150,21 @@
             // numSaldo
             // 
             this.numSaldo.DecimalPlaces = 2;
-            this.numSaldo.Location = new System.Drawing.Point(198, 72);
+            this.numSaldo.Location = new System.Drawing.Point(143, 35);
             this.numSaldo.Maximum = new decimal(new int[] {
             999999999,
             0,
             0,
             0});
             this.numSaldo.Name = "numSaldo";
-            this.numSaldo.Size = new System.Drawing.Size(99, 20);
+            this.numSaldo.Size = new System.Drawing.Size(74, 20);
             this.numSaldo.TabIndex = 2;
             // 
             // txtUserName
             // 
             this.txtUserName.Location = new System.Drawing.Point(16, 35);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(162, 20);
+            this.txtUserName.Size = new System.Drawing.Size(121, 20);
             this.txtUserName.TabIndex = 1;
             this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
             // 
@@ -167,9 +175,9 @@
             this.chNivel,
             this.chSaldo});
             this.lsvUsers.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.lsvUsers.Location = new System.Drawing.Point(6, 118);
+            this.lsvUsers.Location = new System.Drawing.Point(252, 16);
             this.lsvUsers.Name = "lsvUsers";
-            this.lsvUsers.Size = new System.Drawing.Size(433, 131);
+            this.lsvUsers.Size = new System.Drawing.Size(396, 337);
             this.lsvUsers.TabIndex = 0;
             this.lsvUsers.UseCompatibleStateImageBehavior = false;
             this.lsvUsers.View = System.Windows.Forms.View.Details;
@@ -194,24 +202,16 @@
             this.tpJogos.Location = new System.Drawing.Point(4, 22);
             this.tpJogos.Name = "tpJogos";
             this.tpJogos.Padding = new System.Windows.Forms.Padding(3);
-            this.tpJogos.Size = new System.Drawing.Size(445, 288);
+            this.tpJogos.Size = new System.Drawing.Size(654, 374);
             this.tpJogos.TabIndex = 1;
             this.tpJogos.Text = "Jogos";
             this.tpJogos.UseVisualStyleBackColor = true;
-            // 
-            // txtPass
-            // 
-            this.txtPass.Location = new System.Drawing.Point(198, 36);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(100, 20);
-            this.txtPass.TabIndex = 9;
-            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 346);
+            this.ClientSize = new System.Drawing.Size(686, 432);
             this.Controls.Add(this.tbcTabs);
             this.Name = "Form2";
             this.Text = "Form2";
